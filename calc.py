c9aa04e9280cf0ecb98d3c9cff0e0fb629ee6edf
Cool-Katt <69585372+Cool-Katt@users.noreply.github.com>
2021-10-18 09:59:32 +0300

    HW 1_1

---
+++ calc.py
@@ -0,0 +1,14 @@
+number1 = float(input('Please enter a number: '))
+number2 = float(input('Please enter another mumber: '))
+action = input('Chose action: +, -, *, /, q - quit')
+
+while action is not 'q':
+    if action == '+':
+        print(number1 + number2)
+    if action == '-':
+        print(number1 - number2)
+    if action == '*':
+        print(number1 * number2)
+    if action == '/':
+        print(number1 / number2)
+    action = input()
