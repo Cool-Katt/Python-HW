e39c197c804067fd81748bba7499a8182a412c26
Cool-Katt <69585372+Cool-Katt@users.noreply.github.com>
2021-11-01 12:24:56 +0200

    Update num-to-text.py

--- HW3/num-to-text.py
+++ HW3/num-to-text.py
@@ -6,6 +6,8 @@ def print_split(gen):
     for i in range(0, len(str(integer))):
         print(next(gen))
 
+def as_string(func):
+    pass
 
 integer = int(input('Please enter an integer number: '))
 split = generate_numbers(integer)
